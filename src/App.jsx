9b83485fdd09446bd22d341288a0625a4be4f807
Reinhard Wansch <reinhard@wansch.at>
2026-01-15 20:13:24 +0100

    Dropdown: values as parameter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header.jsx"
-import DropdownDifficulty from "./components/DropdownDifficulty.jsx"
+import Dropdown from "./components/Dropdown.jsx"
+
+const DIFFICULTY_LEVELS = ["Easy", "Medium", "Hard"];
 
 function App() {
 
   return (
     <div id="pageCtn" className="min-h-screen px-2 py-3 bg-neutral-900 text-fem-neutral-0">
       <Header pb={92} />
-      <DropdownDifficulty />  
+      <Dropdown values={DIFFICULTY_LEVELS} />  
     </div>
   )
 }
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,34 @@
+import { useState, useRef, useEffect } from "react";
+
+function Dropdown({values}) {
+    const [collapsed, setCollapsed] = useState(true);
+    const [ddValue, setDdValue] = useState("Easy");
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setCollapsed(true);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
+    return (
+        <div ref={dropdownRef}>
+            <p onClick={() => setCollapsed(!collapsed)}>{ddValue}</p>
+            <div id="ddMenu" className={collapsed ? 'hidden' : 'flex flex-col gap-2'}>
+                {values.map(valueI =>
+                    <label key={valueI} htmlFor={valueI} onClick={() => setDdValue(valueI)}>
+                        <input id={valueI} type="radio" name="difficulty" />
+                        {valueI}
+                    </label>)
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Dropdown;
